66862c7c13a897a98bfe4eb5c3577c8c431fd57f
Kokagi-sama <royceton03@gmail.com>
2024-06-21 15:35:51 +0800

    update code

--- mosaic_augmentation.py
+++ mosaic_augmentation.py
@@ -18,6 +18,13 @@ def resize_image(image, target_size):
     """Resize the given image to the target size."""
     return cv2.resize(image, target_size)
 
+def display_image(image):
+    """Display the given image using OpenCV."""
+    print("\nMosaic Augmentation Final Output Image")
+    cv2.imshow('Mosaic Augmentation', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 def compute_coords(x_ratio, y_ratio, output_size):
     """Compute x_coords and y_coords based on partition ratios and output size."""
     if x_ratio >= 1.0 or y_ratio >= 1.0 or x_ratio <= 0 or y_ratio <= 0:
@@ -33,6 +40,44 @@ def compute_coords(x_ratio, y_ratio, output_size):
     y_coords = [(0, y_bound), (0, y_bound), (y_bound, output_size), (y_bound, output_size)]
            
     return x_coords, y_coords
+
+def parse_xml_annotations(xml_path):
+    """Parse XML annotations from Roboflow format."""
+    try:
+        # Read the xml file by passing the relative file path
+        tree = ET.parse(xml_path)
+        
+        # Get the root element of the XML file
+        root = tree.getroot()
+
+        # Initialise empty array to kept all annotations that are needed later.
+        annotations = []
+        
+        # Find all objects within the XML to be annotated
+        for obj in root.findall('object'):
+            xmin = float(obj.find('bndbox/xmin').text)
+            ymin = float(obj.find('bndbox/ymin').text)
+            xmax = float(obj.find('bndbox/xmax').text)
+            ymax = float(obj.find('bndbox/ymax').text)
+            
+            # Add annotations from the XML file to add bounding boxes later
+            annotations.append({'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
+            
+        return annotations
+    
+    except FileNotFoundError:
+        print(f"XML file not found: {xml_path}")
+        return None
+    
+    except Exception as e:
+        print(f"Error parsing XML file {xml_path}: {e}")
+        return None
+
+def draw_bounding_box(image, xmin, ymin, xmax, ymax):
+    """Draw a bounding box on the image."""
+    color = (128,0,128)  # Purple colour rectangle to denote the bounding box
+    thickness = 2 # Thickness of the rectangle
+    return cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, thickness)
 
 def crop_random_region(image, x_start, x_end, y_start, y_end):
     """Crop a random region from the image based on given start and end coordinates."""
@@ -58,13 +103,6 @@ def crop_random_region(image, x_start, x_end, y_start, y_end):
     cropped_region = image[start_y:end_y, start_x:end_x]
     
     return cropped_region
-
-def draw_bounding_box(image, xmin, ymin, xmax, ymax):
-    """Draw a bounding box on the image."""
-    color = (0, 255, 0)  # Green color for the bounding box
-    thickness = 2
-    print(f"Bounding box coordinates: xmin={xmin}, ymin={ymin}, xmax={xmax}, ymax={ymax}")
-    return cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, thickness)
     
 def mosaic_augmentation(image_paths, x_coords, y_coords, output_size=416):
     """Create a mosaic image with variable quadrant sizes by cropping specific regions and drawing bounding boxes."""
@@ -99,8 +137,7 @@ def mosaic_augmentation(image_paths, x_coords, y_coords, output_size=416):
                 ymin = int(annotation['ymin'])
                 ymax = int(annotation['ymax'])
                 original_image = draw_bounding_box(original_image, xmin, ymin, xmax, ymax)
-                print(f"Object xmin: {xmin}, ymin: {ymin}, xmax: {xmax}, ymax: {ymax}")
-
+                
         # Resize the image to match output_size if necessary
         if original_image.shape[0] != output_size or original_image.shape[1] != output_size:
             original_image = resize_image(original_image, (output_size, output_size))
@@ -117,39 +154,6 @@ def mosaic_augmentation(image_paths, x_coords, y_coords, output_size=416):
     
     return mosaic_image
 
-
-def parse_xml_annotations(xml_path):
-    """Parse XML annotations from Roboflow format."""
-    try:
-        print(xml_path)
-        tree = ET.parse(xml_path)
-        root = tree.getroot()
-
-        annotations = []
-        
-        for obj in root.findall('object'):
-            xmin = float(obj.find('bndbox/xmin').text)
-            ymin = float(obj.find('bndbox/ymin').text)
-            xmax = float(obj.find('bndbox/xmax').text)
-            ymax = float(obj.find('bndbox/ymax').text)
-            
-            annotations.append({'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
-            
-        return annotations
-    except FileNotFoundError:
-        print(f"XML file not found: {xml_path}")
-        return None
-    except Exception as e:
-        print(f"Error parsing XML file {xml_path}: {e}")
-        return None
-
-def display_image(image):
-    """Display the given image using OpenCV."""
-    print("\nMosaic Augmentation Final Output Image")
-    cv2.imshow('Mosaic Augmentation', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
 # Directory containing JPEG images and XML annotations
 image_directory = 'images/'
 
@@ -159,6 +163,7 @@ image_paths = glob.glob(os.path.join(image_directory, '*.jpg'))
 # Make sure output_size is a natural number, N >= 0
 output_size = -1
 while output_size < 0:
+    # Take user input of output size
     output_size = int(input("\nInput the output size, e.g. 416 will result in final output being 416x416 image: "))
     if output_size <= 1:
         print("\nInvalid input. Please re-enter a natural number, N >=0.")
@@ -168,6 +173,7 @@ print_boundary()
 # Make sure x_ratio is within its bound, 0.0 < x < 1.0
 x_ratio = -1
 while x_ratio <= 0 or x_ratio >= 1:
+    # Take user input of x_ratio, the variable to calculate the partition of the image later
     x_ratio = float(input("\nInput the partition ratio of horizontal plane/x plane (0.0 < x <1.0)\nE.g. x_ratio = 0.3 will result in 1st and 3rd quadrant occupying 0.3 of the final image and 2nd and 4th quadrant occupying 0.7 of the rest of the final image.: "))
     if x_ratio <= 0 or x_ratio >= 1:
         print("\nInvalid input. Please re-enter a ratio between 0.0 and 1.0.")
@@ -177,6 +183,7 @@ print_boundary()
 # Make sure y_ratio is within its bound, 0.0 < y < 1.0
 y_ratio = -1
 while y_ratio <= 0 or y_ratio >= 1:
+    # Take user input of y_ratio, the variable to calculate the partition of the image later
     y_ratio = float(input("\nInput the partition ratio of vertical plane/y plane (0.0 < y < 1.0)\nE.g. y_ratio = 0.4 will result in 1st and 2nd quadrant occupying 0.4 of the final image and 3rd and 4th quadrant occupying 0.6 of the rest of the final image.: "))
     if y_ratio <= 0 or y_ratio >= 1:
         print("\nInvalid input. Please re-enter a ratio between 0.0 and 1.0.")
